0ae2b559697368f8c0c4b17ab58552db92cad93c
Krit <krit@boonmeelab.com>
2022-04-04 02:04:32 +0700

    rename home schema and remove vision

--- src/api/vision/controllers/vision.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  vision controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::vision.vision');
--- src/api/vision/services/vision.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * vision service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::vision.vision');
