8e7e883fc3feba0e37da8609d885740e89475cd7
Krit <krit@boonmeelab.com>
2022-04-04 03:10:57 +0700

    rename announcements

--- src/api/annoucement/routes/annoucement.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * annoucement router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::annoucement.annoucement');
--- src/api/annoucement/services/annoucement.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * annoucement service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::annoucement.annoucement');
